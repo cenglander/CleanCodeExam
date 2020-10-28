06429479b2ae37241d6b60195e1db7cb48a3e1c2
Charlotte Englander <charlotte.englander@gmail.com>
2020-10-28 13:15:17 +0100

    WIP

---
+++ src/dao/GameDAOJdbcImpl.java
@@ -0,0 +1,27 @@
+package dao;
+
+import java.util.List;
+
+import model.PlayerAverage;
+
+public class GameDAOJdbcImpl implements GameDAO {
+
+	@Override
+	public int getUserByName(String name) {
+		
+		return 0;
+	}
+
+	@Override
+	public void saveResultForUser(int userId, int score) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<PlayerAverage> getTopTen() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
